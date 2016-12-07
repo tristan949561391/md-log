8e44d0c9600f97ec25fa9a245c30a269f6c38228
Tristan <tristan949561391@gmail.com>
2016-12-07 21:23:35 +0800

    添加

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
  */
 const modelInit = require('./lib/LogModel')
 
-exports.info = function (content, type, who) {
-    modelInit.saveLog(null, type, content, who);
+exports.info = function (content, type, who, ip) {
+    modelInit.saveLog(null, type, content, who, ip);
 }
-exports.warn = function (content, type, who) {
-    modelInit.saveLog('warn', type, content, who);
+exports.warn = function (content, type, who, ip) {
+    modelInit.saveLog('warn', type, content, who, ip);
 }
-exports.debug = function (content, type, who) {
-    modelInit.saveLog('debug', type, content, who);
+exports.debug = function (content, type, who, ip) {
+    modelInit.saveLog('debug', type, content, who, ip);
 }
-exports.error = function (content, type, who) {
-    modelInit.saveLog('error', type, content, who);
+exports.error = function (content, type, who, ip) {
+    modelInit.saveLog('error', type, content, who, ip);
 }
 
 
